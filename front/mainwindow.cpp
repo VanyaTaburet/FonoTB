b7062aacf6c8f4652004ec03dd84575beb135a60
Антон Третьяк <oren.04@mail.ru>
2025-07-09 15:57:19 +0300

    The front-end is connected to qt, the interface is made to fit the size of the application window

---
+++ front/mainwindow.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include <QWebEngineView>
+#include <QCoreApplication>
+#include <QResizeEvent>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    auto *view = new QWebEngineView(this);
+    view->load(QUrl::fromLocalFile(QCoreApplication::applicationDirPath() + "/index.html"));
+    setCentralWidget(view);
+
+    setMinimumSize(640, 360);   // минимум 16:9
+    setMaximumSize(1920, 1080); // максимум 16:9
+
+    resize(1280, 720);  // начальный размер 16:9
+}
+
+void MainWindow::resizeEvent(QResizeEvent *event) {
+    QMainWindow::resizeEvent(event);
+    qreal scaleFactor = width() / 1280.0;
+    
+    QFont font = this->font();
+    font.setPointSizeF(12 * scaleFactor);
+    this->setFont(font);
+}
+
+
